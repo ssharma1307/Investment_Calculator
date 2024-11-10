f7d7d191520d7e348d3e417d8885054a62433b89
ssharma1307 <sharma.sukhdeep30@gmail.com>
2024-11-10 17:07:51 -0500

    Added User input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import UserInput from "./components/UserInput.jsx";
 import Header from "./components/Header";
+import Result from "./components/Result";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
+    <>
     <Header/>
+    <UserInput userInput={userInput} onChange={handleChange} />
+    <Result input={userInput} />
+    </>
   )
 }
 
